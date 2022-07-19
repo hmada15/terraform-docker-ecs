6ca80946c04027e6b80b79f1e4404c5384498fb4
hmada15 <31375621+hmada15@users.noreply.github.com>
2022-07-19 19:33:30 +0200

    add cluster values  output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,25 @@
 output "vpc_id" {
-  description = "Id that identifies the VPC"
+  description = "Id of the VPC"
   value       = module.vpc.vpc_id
 }
+
+output "autoscaling_group_arn" {
+  description = "ARN of the Autoscaling group"
+  value       = aws_ecs_capacity_provider.this.arn
+}
+
+output "cluster_name" {
+  description = "Name of the cluster"
+  value       = aws_ecs_cluster.this.name
+}
+
+output "cluster_arn" {
+  description = "ARN of the cluster"
+  value       = aws_ecs_cluster.this.arn
+}
+
+output "cluster_id" {
+  description = "ID of the cluster"
+  value       = aws_ecs_cluster.this.id
+}
+
