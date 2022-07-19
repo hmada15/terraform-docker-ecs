afa35755666d100c22cc8d37de071cb7ee9a5e16
hmada15 <31375621+hmada15@users.noreply.github.com>
2022-07-19 13:40:42 +0200

    Create a VPC

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "vpc_id" {
+  description = "Id that identifies the VPC"
+  value       = module.vpc.vpc_id
+}
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "name" {
+  type    = string
+  default = "example-ecs-components"
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
