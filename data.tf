2d5e66082714e5cf0515e6b1e00ba79bc997e80c
hmada15 <31375621+hmada15@users.noreply.github.com>
2022-07-19 19:35:24 +0200

    Move all the data source to thire file

--- data.tf
+++ data.tf
@@ -1,2 +1,17 @@
 // Get the default AWS region
 data "aws_region" "current" {}
+
+// Get the latest ECS optimized AMI
+data "aws_ssm_parameter" "ecs_optimized_ami" {
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended"
+}
+
+data "template_file" "this" {
+  template = <<-EOT
+    #!/bin/bash
+    cat <<'EOF' >> /etc/ecs/ecs.config
+    ECS_CLUSTER=${var.name}
+    ECS_LOGLEVEL=debug
+    EOF
+  EOT
+}
--- main.tf
+++ main.tf
@@ -14,21 +14,6 @@ module "vpc" {
   tags = var.tags
 }
 
-// Get the latest ECS optimized AMI
-data "aws_ssm_parameter" "ecs_optimized_ami" {
-  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended"
-}
-
-data "template_file" "this" {
-  template = <<-EOT
-    #!/bin/bash
-    cat <<'EOF' >> /etc/ecs/ecs.config
-    ECS_CLUSTER=${var.name}
-    ECS_LOGLEVEL=debug
-    EOF
-  EOT
-}
-
 module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "~> 6.5"
